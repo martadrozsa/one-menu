63b4de552a3da1596d3a97f3c874399a2d8a67ee
Marta Denisczwicz Rozsa <marta.denisczwicz@gmail.com>
2025-05-07 07:35:17 -0300

    Initial commit

---
+++ src/main/java/com/denisczwicz/onemenu/OneMenuApplication.java
@@ -0,0 +1,13 @@
+package com.denisczwicz.onemenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneMenuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneMenuApplication.class, args);
+    }
+
+}
