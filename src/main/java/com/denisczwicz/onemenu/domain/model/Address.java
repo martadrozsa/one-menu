a73ba12a75890cb932219b66f19dd6f26bf3ae46
Marta Denisczwicz Rozsa <marta.denisczwicz@gmail.com>
2025-05-22 08:16:39 -0300

    Add user and address

---
+++ src/main/java/com/denisczwicz/onemenu/domain/model/Address.java
@@ -0,0 +1,10 @@
+package com.denisczwicz.onemenu.domain.model;
+
+public class Address {
+    private String street;
+    private String number;
+    private String city;
+    private String state;
+    private String country;
+    private String postalCode;
+}
---
+++ src/main/java/com/denisczwicz/onemenu/domain/model/User.java
@@ -0,0 +1,10 @@
+package com.denisczwicz.onemenu.domain.model;
+
+public class User {
+    private String name;
+    private String email;
+    private String login;
+    private String password;
+    private String lastUpdate;
+    private Address address;
+}
